f38cf11459e7978c9284eea3000c7c7e5e6fb829
AlexGavalas <alex.gavalas.92@gmail.com>
2022-08-30 17:36:08 +0300

    test: deno deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
+import deno from '@astrojs/deno';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [react(), tailwind(), mdx()],
     output: 'server',
-    adapter: cloudflare(),
+    adapter: deno(),
 });
