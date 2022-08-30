de330d3db171b387e44858526c047a8d7dd40b87
AlexGavalas <alex.gavalas.92@gmail.com>
2022-08-30 20:25:36 +0300

    refactor: static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,9 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import deno from '@astrojs/deno';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [react(), tailwind(), mdx()],
-    output: 'server',
-    adapter: deno(),
 });
