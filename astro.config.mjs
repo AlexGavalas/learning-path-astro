a5f858df82d8a36552f0acc9cbf71fbf2471b329
AlexGavalas <alex.gavalas.92@gmail.com>
2022-08-30 19:26:01 +0300

    fix: cloudflare deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import deno from '@astrojs/deno';
+import cloudflare from '@astrojs/cloudflare';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [react(), tailwind(), mdx()],
     output: 'server',
-    adapter: deno(),
+    adapter: cloudflare({ mode: 'directory' }),
 });
